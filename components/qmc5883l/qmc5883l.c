4dbf525f5a5b12b6724ca2f9730ddfc02c1ffc9d
George Ngigi <ngigegeorge023@gmail.com>
2024-06-17 20:33:05 +0300

    Migrated to UncleRus components for magnetometer and ultrasonic.

--- components/qmc5883l/qmc5883l.c
+++ components/qmc5883l/qmc5883l.c
@@ -1,172 +1,257 @@
-/**
- * @file DFRobot_QMC5883.cpp
- * @brief Compatible with QMC5883 HMC5883 and QMC5883
- * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- * @license     The MIT License (MIT)
- * @author      PengKaixing(kaixing.peng@dfrobot.com)
- * @version  V1.0.0
- * @date  2022-2-23
- * @url https://github.com/DFRobot/DFRobot_QMC5883
+/*
+ * Copyright (c) 2019 Ruslan V. Uss <unclerus@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of itscontributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+/**
+ * @file qmc5883l.c
+ *
+ * ESP-IDF Driver for 3-axis magnetic sensor QMC5883L
+ *
+ * Copyright (c) 2019 Ruslan V. Uss <unclerus@gmail.com>
+ *
+ * BSD Licensed as described in the file LICENSE
+ */
+
+#include <esp_log.h>
+#include <esp_idf_lib_helpers.h>
 #include "qmc5883l.h"
 
-#include <math.h>
+#define I2C_FREQ_HZ 400000 // 400kHz
 
-// equivalent to _writeReg()
-void qmc5883l_master_write_slave_register(uint8_t write_reg, uint8_t *data_wr,
-                                          size_t size) {
-  // Write data to the slave
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, MAGNETOMETER_I2C_ADDRESS << 1 | I2C_MASTER_WRITE,
-                        true);
-  i2c_master_write_byte(cmd, write_reg, true);
-  i2c_master_write(cmd, data_wr, size, true);
-  i2c_master_stop(cmd);
-  i2c_master_cmd_begin(SELECTED_I2C, cmd, 1000 / portTICK_PERIOD_MS);
-  i2c_cmd_link_delete(cmd);
+#define REG_XOUT_L 0x00
+#define REG_XOUT_H 0x01
+#define REG_YOUT_L 0x02
+#define REG_YOUT_H 0x03
+#define REG_ZOUT_L 0x04
+#define REG_ZOUT_H 0x05
+#define REG_STATE  0x06
+#define REG_TOUT_L 0x07
+#define REG_TOUT_H 0x08
+#define REG_CTRL1  0x09
+#define REG_CTRL2  0x0a
+#define REG_FBR    0x0b
+#define REG_ID     0x0d
+
+#define MASK_MODE  0xfe
+#define MASK_ODR   0xf3
+
+static const char *TAG = "qmc5883l";
+
+#define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
+#define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
+
+inline static esp_err_t write_reg_nolock(qmc5883l_t *dev, uint8_t reg, uint8_t val)
+{
+    return i2c_dev_write_reg(&dev->i2c_dev, reg, &val, 1);
 }
 
-void qmc5883l_master_read_slave_register(uint8_t reg_offset, uint8_t *data_rd,
-                                         size_t len) {
-  // Move the register pointer to the offset register to read from
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (MAGNETOMETER_I2C_ADDRESS << 1) | I2C_MASTER_WRITE,
-                        true);
-  i2c_master_write_byte(cmd, reg_offset,
-                        true);  // move the I2C pointer to this register
-  i2c_master_stop(cmd);
-  esp_err_t ret =
-      i2c_master_cmd_begin(SELECTED_I2C, cmd, 1000 / portTICK_PERIOD_MS);
-  if (ret != ESP_OK) {
-    ESP_LOGE(pcTaskGetName(0), "Failed to set address pointer");
-    return;
-  }
-  // Read that address register
-  i2c_cmd_link_delete(cmd);
-  cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (MAGNETOMETER_I2C_ADDRESS << 1) | I2C_MASTER_READ,
-                        true);
-  // if (len > 1) {
-  // i2c_master_read(cmd, data_rd, len - 1, I2C_MASTER_ACK);
-  // }
-  // i2c_master_read_byte(cmd, data_rd + len - 1, I2C_MASTER_NACK);
-  ret = i2c_master_read(cmd, data_rd, len, I2C_MASTER_LAST_NACK);
-  if (ret != ESP_OK) {
-    ESP_LOGE("Register", "Cannot read the content at that register address");
-  }
-  i2c_master_stop(cmd);
-  /*esp_err_t*/ ret =
-      i2c_master_cmd_begin(SELECTED_I2C, cmd, 1000 / portTICK_PERIOD_MS);
-  if (ret != ESP_OK) {
-    printf("I2C read failed: %s\n", esp_err_to_name(ret));
-  }
-  i2c_cmd_link_delete(cmd);
+inline static esp_err_t read_reg_nolock(qmc5883l_t *dev, uint8_t reg, uint8_t *val)
+{
+    return i2c_dev_read_reg(&dev->i2c_dev, reg, val, 1);
 }
 
-bool qmcInit(struct df_qmc5883 *qmc, uint8_t I2C_addr) {
-  // isHMC_ = false;
-  // isQMC_ = false;
-  qmc->minX = 0;
-  qmc->maxX = 0;
-  qmc->minY = 0;
-  qmc->maxY = 0;
-  qmc->minZ = 0;
-  qmc->maxZ = 0;
-  qmc->firstRun = true;
-  // this->_pWire = pWire;
-  qmc->_I2C_addr = I2C_addr;
-  //=============================================================
-  qmc->Gauss_LSB_XY = 1090.0;
+static esp_err_t write_reg(qmc5883l_t *dev, uint8_t reg, uint8_t val)
+{
+    CHECK_ARG(dev);
 
-  // begin()
-  uint8_t reg = 0x01;
-  qmc5883l_master_write_slave_register(QMC5883_REG_IDENT_B, &reg, 1);
-  reg = 0x40;
-  qmc5883l_master_write_slave_register(QMC5883_REG_IDENT_C, &reg, 1);
-  reg = 0x01;
-  qmc5883l_master_write_slave_register(QMC5883_REG_IDENT_D, &reg, 1);
-  reg = 0x1D;
-  qmc5883l_master_write_slave_register(QMC5883_REG_CONFIG_1, &reg, 1);
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, reg, val));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
 
-  // fastRegister8()
-  uint8_t dataB[1];
-  qmc5883l_master_read_slave_register(QMC5883_REG_IDENT_B, dataB, 1);
-  uint8_t dataC[1];
-  qmc5883l_master_read_slave_register(QMC5883_REG_IDENT_B, dataC, 1);
-  uint8_t dataD[1];
-  qmc5883l_master_read_slave_register(QMC5883_REG_IDENT_B, dataD, 1);
-  if (dataB[0] != 0x01 || dataC[0] != 0x40 || dataD[0] != 0x01) {
-    return false;
-  }
-  // set range
-  setRange(qmc, QMC5883_RANGE_8GA);
-  // set measurement mode
-  setMeasurementMode(qmc, QMC5883_CONTINUOUS);
-  // set fatarate
-  setDataRate(QMC5883_DATARATE_50HZ);
-  qmc->mgPerDigit = 4.35;
-  return true;
+    return ESP_OK;
 }
 
-void setMeasurementMode(struct df_qmc5883 *qmc, eMode_t mode) {
-  uint8_t value;
-  qmc5883l_master_read_slave_register(QMC5883_REG_CONFIG_1, &value, 1);
-  value &= 0xFC;
-  value |= mode;
-  qmc5883l_master_write_slave_register(QMC5883_REG_CONFIG_1, value, 1);
-}
-void setRange(struct df_qmc5883 *qmc, eRange_t range) {
-  if (range == QMC5883_RANGE_2GA) {
-    qmc->mgPerDigit = 1.22f;
-  } else if (range == QMC5883_RANGE_8GA) {
-    qmc->mgPerDigit = 4.35f;
-  }
-  qmc5883l_master_write_slave_register(QMC5883_REG_CONFIG_2, range << 4, 1);
-}
-void setDataRate(eDataRate_t dataRate) {
-  uint8_t value;
-  qmc5883l_master_read_slave_register(QMC5883_REG_CONFIG_1, &value, 1);
-  value &= 0xF3;
-  value |= (dataRate << 2);
-  qmc5883l_master_write_slave_register(QMC5883_REG_CONFIG_1, value, 1);
-}
-void setSamples(eSamples_t samples) {
-  uint8_t value;
-  qmc5883l_master_read_slave_register(QMC5883_REG_CONFIG_1, &value, 1);
-  value &= 0x3F;
-  value |= (samples << 6);
-  qmc5883l_master_write_slave_register(QMC5883_REG_CONFIG_1, value, 1);
+static esp_err_t read_reg(qmc5883l_t *dev, uint8_t reg, uint8_t *val)
+{
+    CHECK_ARG(dev && val);
+
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, reg, val));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+
+    return ESP_OK;
 }
 
-void setDeclinationAngle(struct df_qmc5883 *qmc, float declinationAngle) {
-  qmc->ICdeclinationAngle = declinationAngle;
+///////////////////////////////////////////////////////////////////////////////
+
+esp_err_t qmc5883l_init_desc(qmc5883l_t *dev, uint8_t addr, i2c_port_t port, gpio_num_t sda_gpio, gpio_num_t scl_gpio)
+{
+    CHECK_ARG(dev);
+
+    dev->i2c_dev.port = port;
+    dev->i2c_dev.addr = addr;
+    dev->i2c_dev.cfg.sda_io_num = sda_gpio;
+    dev->i2c_dev.cfg.scl_io_num = scl_gpio;
+#if HELPER_TARGET_IS_ESP32
+    dev->i2c_dev.cfg.master.clk_speed = I2C_FREQ_HZ;
+#endif
+    return i2c_dev_create_mutex(&dev->i2c_dev);
 }
 
-void getHeadingDegrees(struct df_qmc5883 *qmc) {
-  float heading = atan2(qmc->v.YAxis, qmc->v.XAxis);
-  heading += qmc->ICdeclinationAngle;
-  if (heading < 0) heading += 2 * PI;
-  if (heading > 2 * PI) heading -= 2 * PI;
-  qmc->v.HeadingDegrees = heading * 180 / PI;
+esp_err_t qmc5883l_free_desc(qmc5883l_t *dev)
+{
+    CHECK_ARG(dev);
+
+    return i2c_dev_delete_mutex(&dev->i2c_dev);
 }
 
-void readRaw(struct df_qmc5883 *qmc) {
-  uint8_t data[6];
-  qmc5883l_master_read_slave_register(QMC5883_REG_OUT_X_L, data, 6);
-  qmc->v.XAxis = (uint16_t)(data[1] << 8) | data[0];
-  qmc->v.YAxis = (uint16_t)(data[3] << 8) | data[2];
-  qmc->v.ZAxis = (uint16_t)(data[5] << 8) | data[4];
+esp_err_t qmc5883l_reset(qmc5883l_t *dev)
+{
+    CHECK(write_reg(dev, REG_CTRL2, 0x80));
+    dev->range = QMC5883L_RNG_2;
 
-  qmc->v.AngleXY =
-      (atan2((double)qmc->v.YAxis, (double)qmc->v.XAxis) * (180 / 3.14159265) +
-       180);
-  qmc->v.AngleXZ =
-      (atan2((double)qmc->v.ZAxis, (double)qmc->v.XAxis) * (180 / 3.14159265) +
-       180);
-  qmc->v.AngleYZ =
-      (atan2((double)qmc->v.ZAxis, (double)qmc->v.YAxis) * (180 / 3.14159265) +
-       180);
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_get_chip_id(qmc5883l_t *dev, uint8_t *id)
+{
+    return read_reg(dev, REG_ID, id);
+}
+
+esp_err_t qmc5883l_set_mode(qmc5883l_t *dev, qmc5883l_mode_t mode)
+{
+    CHECK_ARG(dev && mode <= QMC5883L_MODE_CONTINUOUS);
+
+    uint8_t v;
+
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, REG_CTRL1, &v));
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_CTRL1, (v & 0xfe) | mode));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_get_mode(qmc5883l_t *dev, qmc5883l_mode_t *mode)
+{
+    CHECK_ARG(mode);
+
+    uint8_t v;
+    CHECK(read_reg(dev, REG_CTRL1, &v));
+    *mode = v & 1;
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_set_config(qmc5883l_t *dev, qmc5883l_odr_t odr, qmc5883l_osr_t osr, qmc5883l_range_t rng)
+{
+    CHECK_ARG(dev && odr <= QMC5883L_DR_200 && osr <= QMC5883L_OSR_512 && rng <= QMC5883L_RNG_8);
+
+    uint8_t v;
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, REG_CTRL1, &v));
+    dev->range = rng;
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_FBR, 1)); // Define set/reset period
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_CTRL1,
+            (v & 0x03) | ((odr & 3) << 2) | ((rng & 1) << 4) | ((osr & 3) << 6)));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_get_config(qmc5883l_t *dev, qmc5883l_odr_t *odr, qmc5883l_osr_t *osr, qmc5883l_range_t *rng)
+{
+    CHECK_ARG(odr && osr && rng);
+
+    uint8_t v;
+    CHECK(read_reg(dev, REG_CTRL1, &v));
+    *odr = (v >> 2) & 3;
+    *osr = (v >> 6) & 3;
+    *rng = (v >> 4) & 1;
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_set_int(qmc5883l_t *dev, bool enable)
+{
+    return write_reg(dev, REG_CTRL2, enable ? 1 : 0);
+}
+
+esp_err_t qmc5883l_get_int(qmc5883l_t *dev, bool *enable)
+{
+    CHECK_ARG(enable);
+
+    uint8_t v;
+    CHECK(read_reg(dev, REG_CTRL2, &v));
+    *enable = v & 1;
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_data_ready(qmc5883l_t *dev, bool *ready)
+{
+    CHECK_ARG(ready);
+
+    uint8_t v;
+    CHECK(read_reg(dev, REG_STATE, &v));
+    *ready = v & 1;
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_get_raw_data(qmc5883l_t *dev, qmc5883l_raw_data_t *raw)
+{
+    CHECK_ARG(dev && raw);
+
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    esp_err_t ret = i2c_dev_read_reg(&dev->i2c_dev, REG_XOUT_L, raw, 6);
+    if (ret != ESP_OK)
+        ESP_LOGE(TAG, "Could not read data register, err = %d", ret);
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+    return ret;
+}
+
+esp_err_t qmc5883l_raw_to_mg(qmc5883l_t *dev, qmc5883l_raw_data_t *raw, qmc5883l_data_t *data)
+{
+    CHECK_ARG(dev && raw && data);
+
+    float f = (dev->range == QMC5883L_RNG_2 ? 2000.0 : 8000.0) / 32768;
+
+    data->x = raw->x * f;
+    data->y = raw->y * f;
+    data->z = raw->z * f;
+
+    return ESP_OK;
+}
+
+esp_err_t qmc5883l_get_data(qmc5883l_t *dev, qmc5883l_data_t *data)
+{
+    qmc5883l_raw_data_t raw;
+    CHECK(qmc5883l_get_raw_data(dev, &raw));
+    return qmc5883l_raw_to_mg(dev, &raw, data);
+}
+
+esp_err_t qmc5883l_get_raw_temp(qmc5883l_t *dev, int16_t *temp)
+{
+    CHECK_ARG(dev && temp);
+
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    esp_err_t ret = i2c_dev_read_reg(&dev->i2c_dev, REG_TOUT_L, temp, 2);
+    if (ret != ESP_OK)
+        ESP_LOGE(TAG, "Could not read TOUT register, err = %d", ret);
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+    return ret;
 }
--- main/main.c
+++ main/main.c
@@ -1,10 +1,14 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #include "m_motor.h"
 #include "m_utils.h"
 #include "qmc5883l.h"
+#include "ultrasonic.h"
 
+void getHeadingDegrees(qmc5883l_data_t *raw, float icDeclinitionAngle,
+                       float *headingDegrees);
 void app_main(void) {
   struct motor leftMotor = {
       .in1Pin = LEFT_MOTOR_IN1,
@@ -26,27 +30,82 @@ void app_main(void) {
       .generator = MCPWM_GEN_A,
       .signals = MCPWM1A,
   };
-
-  i2cInit();
-  struct df_qmc5883 qmc;
-  qmcInit(&qmc, MAGNETOMETER_I2C_ADDRESS);
-
-  // Formula: (deg + (min / 60.0)) / (180 / PI);
-  float declinationAngle = (0 + (39.0 / 60.0)) / (180 / PI);
-  setDeclinationAngle(&qmc, declinationAngle);
-
   initMotors(&leftMotor);
   initMotors(&rightMotor);
 
-  while (true) {
-    readRaw(&qmc);
-    getHeadingDegrees(&qmc);
-    float headingDegrees = qmc.v.HeadingDegrees;
+  // i2cInit();
+  i2c_dev_t i2c_dev = {
+      .port = I2C_NUM_0,
+      .addr = QMC5883L_I2C_ADDR_DEF,
+      .cfg =
+          {
+              .sda_io_num = I2C_SDA_PIN,
+              .scl_io_num = I2C_SCL_PIN,
+              .master =
+                  {
+                      .clk_speed = 100000,
+                  },
+          },
+  };
+  ESP_ERROR_CHECK(i2cdev_init());
+  qmc5883l_t qmc = {
+      .i2c_dev = i2c_dev,
+      .range = QMC5883L_RNG_8,
+  };
+  ESP_ERROR_CHECK(qmc5883l_init_desc(&qmc, QMC5883L_I2C_ADDR_DEF, I2C_NUM_0,
+                                     I2C_SDA_PIN, I2C_SCL_PIN));
+  ESP_ERROR_CHECK(qmc5883l_reset(&qmc));
+  ESP_ERROR_CHECK(qmc5883l_set_mode(&qmc, QMC5883L_MODE_CONTINUOUS));
 
-    ESP_LOGW("MAIN", "[DIR]: %f", headingDegrees);
+  ultrasonic_sensor_t ultrasonic_sensor = {
+      .trigger_pin = 16,
+      .echo_pin = 17,
+  };
+  ESP_ERROR_CHECK(ultrasonic_init(&ultrasonic_sensor));
+
+  bool dataReady = false;
+  float distance = 0;
+  while (true) {
+    esp_err_t ret = ultrasonic_measure(&ultrasonic_sensor, 4, &distance);
+    // if (ret == ESP_OK) {
+    //   ESP_LOGW("MAIN", "Distance: %f cm", distance);
+    // }
+
+    ESP_ERROR_CHECK(qmc5883l_data_ready(&qmc, &dataReady));
+    if (!dataReady) {
+      // vTaskDelay(0 / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    qmc5883l_data_t raw;
+    ESP_ERROR_CHECK(qmc5883l_get_data(&qmc, &raw));
+    float heading = 0;
+    float inclination = (0 + (39.0 / 60.0)) / (180 / M_PI);
+
+    getHeadingDegrees(&raw, inclination, &heading);
+    if (ret == ESP_OK) {
+      ESP_LOGW("MAIN", "[RAW]: x=%f, y=%f, z=%f, dir:%f Obstacle: %f", raw.x,
+               raw.y, raw.z, heading, distance * 100);
+    } else {
+      ESP_LOGW("MAIN", "[RAW]: x=%f, y=%f, z=%f, dir:%f", raw.x, raw.y, raw.z,
+               heading);
+    }
 
     setMotorSpeed(&rightMotor, 4095);
     setMotorSpeed(&leftMotor, -3276);
+
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
+}
+void getHeadingDegrees(qmc5883l_data_t *raw, float icDeclinationAngle,
+                       float *headingDegrees) {
+  // FIXME: This function is not working properly. Will add accelerometer
+  float x = raw->x, y = raw->y;
+  float heading = atan2(x, y);
+  heading += icDeclinationAngle;
+
+  if (heading < 0) heading += 2 * M_PI;
+  if (heading > 2 * M_PI) heading -= 2 * M_PI;
+
+  *headingDegrees = heading * (180.0 / M_PI);
 }
